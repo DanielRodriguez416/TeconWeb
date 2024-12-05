d199b4e0f32d4980fa571fd5fe76170cf930fd4f
David Gonzalez PC_DC <david.gonzalez@tecon.com.co>
2024-12-04 22:29:33 -0500

    changes authLogin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey', // Cambiar a una variable de entorno
+      signOptions: { expiresIn: '1h' }, // Token válido por 1 hora
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  private users = [
+    { username: 'David', password: bcrypt.hashSync('1234', 10) }, // Ejemplo
+  ];
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = this.users.find((user) => user.username === username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result; // Retorna el usuario sin la contraseña
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username };
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+}
